7931ceb6a4a0279dbbe487d479c8aa2cf6173b29
Arthur S. Miertschink <arthursm50@gmail.com>
2021-01-10 10:59:28 -0300

    Alterando o projeto para receber migrations

--- RestWithASPNET5/RestWithASPNET5/Startup.cs
+++ RestWithASPNET5/RestWithASPNET5/Startup.cs
@@ -16,27 +16,41 @@ using RestWithASPNET5.Model.Context;
 using RestWithASPNET5.Bussiness.Implementations;
 using RestWithASPNET5.Repository;
 using RestWithASPNET5.Repository.Implementations;
+using Serilog;
 
 namespace RestWithASPNET5
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get;  }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration()
+                 .WriteTo.Console()
+                 .CreateLogger();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
 
             services.AddControllers();
 
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
 
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
             // Versioning API
             services.AddApiVersioning();
 
@@ -49,6 +63,8 @@ namespace RestWithASPNET5
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithASPNET5", Version = "v1" });
             });
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -70,6 +86,24 @@ namespace RestWithASPNET5
             {
                 endpoints.MapControllers();
             });
+        }      
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database Migration Failed", ex);
+                throw;
+            }
         }
     }
 }
---
+++ RestWithASPNET5/RestWithASPNET5/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO `person` (`id`, `address`, `firstname`, `gender`, `last_name`) VALUES
+	(1, 'SMJ', 'Arthur', 'Male', 'Miertschink'),
+	(2, 'Quebrada', 'Jose', 'Male', 'Da vinti'),
+	(3, 'Morro do leste', 'Kleber', 'Male', 'Tudo dois'),
+	(4, 'Adiresson', 'Juana', 'Female', 'Andirova');
